8b28800d398d85c1aa0922e3215744838f008e77
Zayed Ahmed <zayednasck@gmail.com>
2026-09-16 09:32:26 +0600

    Increment-2 completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 
+//done with increment 2
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
